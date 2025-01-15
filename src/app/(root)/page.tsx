b99f98185ae8c89d525f9d870a001104b4df0f46
muhammadsami <muhammadsami1242@gmail.com>
2025-01-16 00:45:29 +0500

    second commit

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="text-orange-500 text-5xl font-bold">
+        Welcome to my Website
+      </div>
+    </main>
+  );
+}
